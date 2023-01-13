34b7ac0034fbdb1b8336bb108b446ebc385ba06b
Ajit Kumar Panda <ajit.kumar.panda@sap.com>
2023-01-13 09:14:06 +0000

    fix errors

--- cap-media/srv/service.js
+++ cap-media/srv/service.js
@@ -6,26 +6,39 @@ module.exports = cds.service.impl(async function (srv) {
   const { MediaFiles } = this.entities;
 
   srv.on('PUT', MediaFiles, async (req, next) => {
+    const url = req._.req.path, chunks = [];
+    if (url.includes('content')) {
 
-    let mediaMalwareScanner = function (req) {
-      let malwarescan = new Promise(function (resolve, reject) {
-        const stream = new PassThrough(), chunks = []
-        stream.on('data', chunk => chunks.push(chunk));
-        stream.on('end', async (req) => {
-          let body = Buffer.concat(chunks).toString('binary');
-          let requestBuilder = await apiMalwareScan.DefaultApi.createScan(body);
-          requestBuilder.addCustomHeaders({ 'Content-Type': 'application/octet-stream' });
-          requestBuilder.addCustomHeaders({ 'Accept': 'application/json' });
-          let response = await requestBuilder.skipCsrfTokenFetching().execute({ destinationName: 'cap-media-malwarescanner-dest' });
-          resolve(response.malwareDetected);
+      let mediaMalwareScanner = function (req) {
+        let malwarescan = new Promise(function (resolve, reject) {
+          req.data.content.on('data', chunk => chunks.push(chunk));
+          req.data.content.on('end', async (req) => {
+            let body = Buffer.concat(chunks).toString('binary');
+            let requestBuilder = await apiMalwareScan.DefaultApi.createScan(body);
+            requestBuilder.addCustomHeaders({ 'Content-Type': 'application/octet-stream' });
+            requestBuilder.addCustomHeaders({ 'Accept': 'application/json' });
+            let response = await requestBuilder.skipCsrfTokenFetching().execute({ destinationName: 'cap-media-malwarescanner-dest' });
+            resolve(response.malwareDetected);
+          });
         });
-        req.data.content.pipe(stream);
-      });
-      return malwarescan;
-    }
+        return malwarescan;
+      }
 
-    let isMalwareDetected = await mediaMalwareScanner(req);
-    isMalwareDetected ? req.error(500, 'Malware Detected') : next();
+      let isMalwareDetected = await mediaMalwareScanner(req);
+      if (isMalwareDetected) {
+        req.error(500, 'Malware Detected');
+      } else {
+        req.data.content = new PassThrough();
+        req.data.content.push(Buffer.concat(chunks));
+        req.data.content.push(null);
+        next()
+      }
+
+    } else {
+
+      return next();
+
+    }
 
   });
 
